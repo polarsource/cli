9162ff68c9ce6e771423724c222255b384fbe5a7
Emil Widlund <hello@emilwidlund.com>
2025-08-13 14:43:11 +0200

    adjust migration

--- src/commands.ts
+++ src/commands.ts
@@ -1,24 +1,35 @@
 import { Command, Options } from "@effect/cli";
-import { Console } from "effect";
+import { Console, Effect } from "effect";
 import { Option } from "effect";
+import { Migration } from "./services/migrate";
 
-const migrationProvider = Options.choice("provider", [
+export const migrationProvider = Options.choice("provider", [
   "lemonSqueezy",
   "paddle",
   "stripe",
 ]).pipe(Options.withAlias("p"), Options.optional);
 
+export const migrationAPIKey = Options.redacted("migrationAPIKey");
+
 export const migrate = Command.make(
   "migrate",
-  { migrationProvider },
-  ({ migrationProvider }) => {
-    const provider = Option.match(migrationProvider, {
-      onSome: (provider) => provider,
-      onNone: () => null,
-    });
+  { migrationProvider, migrationAPIKey },
+  ({ migrationProvider, migrationAPIKey }) =>
+    Effect.gen(function* () {
+      const provider = Option.match(migrationProvider, {
+        onSome: (provider) => provider,
+        onNone: () => null,
+      });
 
-    return Console.log("Test", provider);
-  }
+      yield* Effect.log(provider, migrationAPIKey);
+
+      const migration = yield* Migration;
+
+      const products = yield* migration.products();
+      const customers = yield* migration.customers();
+
+      return Console.log(products, customers);
+    })
 );
 
 export const LemonSqueezyAPIKey = Options.redacted("lemonSqueezyAPIKey");
