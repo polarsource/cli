ab28bf7c8ef633f9832f0a2789a70aea530d17a6
Emil Widlund <hello@emilwidlund.com>
2025-08-19 11:09:41 +0200

    cleanup organization prompt

--- src/prompts/organizations.ts
+++ src/prompts/organizations.ts
@@ -4,6 +4,29 @@ import { OrganizationCreate } from "../schemas/Organization";
 import * as Polar from "../services/polar";
 import { slugify } from "../utils";
 
+const selectOrganizationPrompt = Effect.gen(function* () {
+  const polar = yield* Polar.Polar;
+  const organizations = yield* polar
+    .use((client) =>
+      client.organizations.list({
+        page: 1,
+        limit: 100,
+      })
+    )
+    .pipe(Effect.map((organizations) => organizations.result.items));
+
+  return yield* Prompt.select({
+    message: "Select Organization",
+    choices: [
+      ...organizations.map((organization) => ({
+        value: organization.id,
+        title: organization.name,
+      })),
+      { value: "new", title: "+ Create New Organization" },
+    ],
+  });
+});
+
 const organizationNamePrompt = Prompt.text({
   message: "Organization Name",
 });
@@ -15,53 +38,28 @@ const organizationSlugPrompt = (name: string) =>
   });
 
 const createNewOrganizationPrompt = Effect.gen(function* () {
+  const polar = yield* Polar.Polar;
   const name = yield* organizationNamePrompt;
   const slug = yield* organizationSlugPrompt(name);
 
-  return yield* Schema.decode(OrganizationCreate)({ name, slug });
-}).pipe(
-  Effect.flatMap((organizationCreate) =>
-    Effect.gen(function* () {
-      const polar = yield* Polar.Polar;
-      const organization = yield* polar.use((client) =>
-        client.organizations.create(organizationCreate)
-      );
+  const organizationCreate = yield* Schema.decode(OrganizationCreate)({
+    name,
+    slug,
+  });
 
-      return organization.id;
-    })
-  )
-);
-
-export const OrganizationPrompt = Effect.gen(function* () {
-  const polar = yield* Polar.Polar;
-  const organizations = yield* polar
-    .use((client) =>
-      client.organizations.list({
-        page: 1,
-        limit: 100,
-      })
-    )
-    .pipe(Effect.map((organizations) => organizations.result.items));
-
-  const organization = yield* Prompt.select({
-    message: "Select Organization",
-    choices: [
-      ...organizations.map((organization) => ({
-        value: organization.id,
-        title: organization.name,
-      })),
-      { value: "new", title: "+ Create New Organization" },
-    ] as const,
-  }).pipe(
-    Prompt.run,
-    Effect.flatMap((organization) => {
-      if (organization === "new") {
-        return createNewOrganizationPrompt;
-      }
-
-      return Effect.succeed(organization);
-    })
+  const organization = yield* polar.use((client) =>
+    client.organizations.create(organizationCreate)
   );
 
-  return organization;
+  return organization.id;
 });
+
+export const OrganizationPrompt = selectOrganizationPrompt.pipe(
+  Effect.flatMap((organization) => {
+    if (organization === "new") {
+      return createNewOrganizationPrompt;
+    }
+
+    return Effect.succeed(organization);
+  })
+);
